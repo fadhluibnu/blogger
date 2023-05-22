f77fdd2241740eefa01c1d58719f18bd2a061631
fadhluibnu <f.ibnu.a1@gmail.com>
2023-05-22 10:01:12 +0700

    data tutorial migrations

---
+++ backend/database/migrations/2023_05_22_012119_create_data_tutorials_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_tutorials', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_playlist');
+            $table->foreignId('id_post');
+            $table->integer('playlist_order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_tutorials');
+    }
+};
